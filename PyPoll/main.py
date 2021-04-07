46b477af9e954019811f59eee0dbd5a0cf8f37bc
KALI HULLSIEK <hullsiek@gmail.com>
2021-04-06 21:31:49 -0500

    working on poll data

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,8 +1,8 @@
 import os
-
-csvpath = os.path.join('..', 'Resources', 'election_data.csv')
-
+import pandas as pd
 import csv
+
+csvpath = os.path.join('/Users/hulls/Desktop/GitHub/Python-challenge/python-challenge/PyPoll/Resources', 'election_data.csv')
 
 with open(csvpath) as poll_data:
 
@@ -10,4 +10,35 @@ with open(csvpath) as poll_data:
 
     csv_header = next(csvreader)
 
-    print(poll_data)
+    total_votes = []
+    candidate_list = []
+    vote_count = []
+
+    for row in csvreader:
+
+        total_votes.append(str(row[0]))
+
+        candidate = str(row[2])
+
+
+        if candidate not in candidate_list:
+            candidate_list.append(candidate)
+
+#print(f"List of candidates: {candidate_list}")
+
+khan_votes = candidate.count(candidate_list[0])
+correy_votes = candidate.count(candidate_list[1])
+li_votes = candidate.count(candidate_list[2])
+otooley_votes = candidate.count(candidate_list[3])
+
+    
+
+
+print(f"Total Votes: {(len(total_votes))}")
+print(f"v: {candidate_list[0]} {khan_votes}")
+print(f"v: {candidate_list[1]} {correy_votes}")
+print(f"v: {candidate_list[2]} {li_votes}")
+print(f"v: {candidate_list[3]} {otooley_votes}")
+
+
+

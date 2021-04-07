f1fb6cd5583a959ddfb1ce486e8b840b0f27b85d
KALI HULLSIEK <hullsiek@gmail.com>
2021-04-07 14:39:03 -0500

    comments added to code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,12 @@
+#First we must import our csv file and import it
+
 import os
 
 csvpath = os.path.join('c:/Users/hulls/Desktop/GitHub/Python-challenge/python-challenge/PyBank/Resources', 'budget_data.csv')
 
 import csv
 
+#Now we can pull the headers from our csv file to use in our code and skip the header when looking at the collumn values
 
 with open(csvpath) as bank_data:
 
@@ -11,51 +14,72 @@ with open(csvpath) as bank_data:
 
     csv_header = next(csvreader)
 
-    
+#We need to create an empty list for total_months, total_profits, monthly_change
+
     total_months = []
     
     total_profit = []
     
-    monthly_change =[]
+    monthly_change = []
 
-    
+    #Look inside the rows in our open csv
+
     for row in csvreader:
+
+        #We need to add values within the rows from column 0 to our empty list for total_months
        
         total_months.append(str(row[0]))
         
+        #We need to add values within the rows from column 1 to our empty list for total profit
+
         total_profit.append(int(row[1]))
 
+    #now we need to use a variable (x) to retrieve our total_profit list values
     
     for x in range(1, len(total_profit)):
 
+        #We need to take one value in the list and subtract by the row before it to find the change in the value, and add that to our monthly_change list
+
         monthly_change.append((int(total_profit[x]) - int(total_profit[x-1])))
+    
+        #test this is working properly
 
         #print(f"{monthly_change}")
     
-    
-    average_profit_change = sum(monthly_change) / len(monthly_change)
+    #We need to find the average monthly changes in profit, so we add the changes and divide by number of months
+
+    average_profit_change = sum(monthly_change) / len(total_months)
         
     #print(f"{average_profit_change}")
+
+    #We can use our monthly change list to find max change
 
     greatest_increase = max(monthly_change)
 
     #print(f"{greatest_increase}")
 
+    #We can use our monthly change list to find min change
+
     greatest_decrease = min(monthly_change)
 
     #print(f"{greatest_decrease}")
+
+    #We need to find the value of the row next to the max increase to show the month it happened in, we can use index for this
 
     i = monthly_change.index(greatest_increase)
     greatest_increase_month = total_months[i+1]
 
     #print(f"{i} {greatest_increase_month}")
 
+    #We need to find the value of the row next to the max decrease to show the month it happened in, we can use index for this
+
     d = monthly_change.index(greatest_decrease)
     greatest_decrease_month = total_months[d+1]
     
     #print(f"{d} {greatest_decrease_month}")
 
-    
+    #print the results
+
     print("Financial Analysis")
     
     print("-------------------------------------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,3 +1,5 @@
+#We need to import the csv file and module for pandas
+
 import os
 
 import pandas as pd
@@ -7,8 +9,11 @@ import csv
 
 csvpath = os.path.join('/Users/hulls/Desktop/GitHub/Python-challenge/python-challenge/PyPoll/Resources', 'election_data.csv')
 
+#We need to have pandas open read csv file
+
 poll_data_df = pd.read_csv(csvpath)
 
+#We need to set open csv file as reader and grab headers
 
 with open(csvpath) as poll_data:
 
@@ -16,28 +21,38 @@ with open(csvpath) as poll_data:
 
     csv_header = next(csvreader)
 
+    #Within our open csv file we need to create lists for total votes, cadidate list and vote count
+
     total_votes = []
 
     candidate_list = []
 
     vote_count = []
 
+    #Within our rows, we want to take the values of collumns 0 and return it to the list for total votes
 
     for row in csvreader:
 
         total_votes.append(str(row[0]))
 
+        #We want to say that candidate values are the rows within column 2
         candidate = str(row[2])
 
-
+        #We want to say that if our value of candidate that we look at in the 1st row of this column is not in our candidate list, 
+            # then add it to the candidate list (so we don't get duplicate names). It will do this for each row, and skip the duplicates.
         if candidate not in candidate_list:
            
             candidate_list.append(candidate)
 
+#Test that this worked
+
 #print(f"List of candidates: {candidate_list}")
 
+#We can use the pandas module to find how many times a candidate value is listed, which shows the number of votes for that candidate
 
 vote_count = poll_data_df["Candidate"].value_counts()
+
+#We can use the pandas module to find the percentage of counts for each candidate, and use formatting so it will print the way we want it later
 
 percentages = poll_data_df["Candidate"].value_counts(normalize=True).mul(100).round(3).astype(str) + '%'
 
@@ -45,10 +60,14 @@ percentages = poll_data_df["Candidate"].value_counts(normalize=True).mul(100).ro
 
 #print(f"{vote_count}")
 
+#We can use pandas module to find the candidate with the max amount of times their name is listed to show the winner
+
 winner = poll_data_df["Candidate"].value_counts().idxmax()
 
 #print(f"{winner}")
 
+
+#print the results
 
 print(f"Election Results")
 

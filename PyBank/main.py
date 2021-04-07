3a44170a5fa431ae94352f961ee65ed84a075bc5
KALI HULLSIEK <hullsiek@gmail.com>
2021-04-07 13:59:38 -0500

    Files cleaned up

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,6 +4,7 @@ csvpath = os.path.join('c:/Users/hulls/Desktop/GitHub/Python-challenge/python-ch
 
 import csv
 
+
 with open(csvpath) as bank_data:
 
     csvreader = csv.reader(bank_data, delimiter=',')
@@ -12,7 +13,9 @@ with open(csvpath) as bank_data:
 
     
     total_months = []
+    
     total_profit = []
+    
     monthly_change =[]
 
     
@@ -27,19 +30,31 @@ with open(csvpath) as bank_data:
 
         monthly_change.append((int(total_profit[x]) - int(total_profit[x-1])))
 
+        #print(f"{monthly_change}")
+    
     
     average_profit_change = sum(monthly_change) / len(monthly_change)
+        
+    #print(f"{average_profit_change}")
 
     greatest_increase = max(monthly_change)
 
+    #print(f"{greatest_increase}")
+
     greatest_decrease = min(monthly_change)
+
+    #print(f"{greatest_decrease}")
 
     i = monthly_change.index(greatest_increase)
     greatest_increase_month = total_months[i+1]
 
+    #print(f"{i} {greatest_increase_month}")
+
     d = monthly_change.index(greatest_decrease)
     greatest_decrease_month = total_months[d+1]
     
+    #print(f"{d} {greatest_decrease_month}")
+
     
     print("Financial Analysis")
     
@@ -47,9 +62,9 @@ with open(csvpath) as bank_data:
     
     print(f"Total Months: {(len(total_months))}")
     
-    print(f"Total: {sum(total_profit)}")
+    print(f"Total: ${sum(total_profit)}")
     
-    print(f"Average: {average_profit_change}")
+    print(f"Average Change: ${round(average_profit_change, 2)}")
     
     print(f"Greatest Increase in Profits: {greatest_increase_month} (${greatest_increase})")
     
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,10 +1,14 @@
 import os
+
 import pandas as pd
+
 import csv
+
 
 csvpath = os.path.join('/Users/hulls/Desktop/GitHub/Python-challenge/python-challenge/PyPoll/Resources', 'election_data.csv')
 
 poll_data_df = pd.read_csv(csvpath)
+
 
 with open(csvpath) as poll_data:
 
@@ -13,7 +17,9 @@ with open(csvpath) as poll_data:
     csv_header = next(csvreader)
 
     total_votes = []
+
     candidate_list = []
+
     vote_count = []
 
 
@@ -25,6 +31,7 @@ with open(csvpath) as poll_data:
 
 
         if candidate not in candidate_list:
+           
             candidate_list.append(candidate)
 
 #print(f"List of candidates: {candidate_list}")
@@ -32,7 +39,8 @@ with open(csvpath) as poll_data:
 
 vote_count = poll_data_df["Candidate"].value_counts()
 
-percentages = poll_data_df["Candidate"].value_counts(normalize=True).mul(100).round(1).astype(str) + '%'
+percentages = poll_data_df["Candidate"].value_counts(normalize=True).mul(100).round(3).astype(str) + '%'
+
 #print(f"{percentages}")
 
 #print(f"{vote_count}")
@@ -43,13 +51,23 @@ winner = poll_data_df["Candidate"].value_counts().idxmax()
 
 
 print(f"Election Results")
+
 print(f"---------------------------------")
+
 print(f"Total Votes: {(len(total_votes))}")
+
 print(f"---------------------------------")
+
 print(f"{candidate_list[0]}: {percentages[0]} ({vote_count[0]})")
+
 print(f"{candidate_list[1]}: {percentages[1]} ({vote_count[1]})")
+
 print(f"{candidate_list[2]}: {percentages[2]} ({vote_count[2]})")
+
 print(f"{candidate_list[3]}: {percentages[3]} ({vote_count[3]})")
+
 print(f"---------------------------------")
+
 print(f"Winner: {winner}")
+
 print(f"---------------------------------")
